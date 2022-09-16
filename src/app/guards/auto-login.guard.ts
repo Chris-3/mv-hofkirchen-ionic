09a79a7b3e893c4f939bde98f48d932a085e203d
chris-3 <choeftberger@gmail.com>
2022-09-16 10:43:22 +0200

    added autologin

--- src/app/guards/auto-login.guard.ts
+++ src/app/guards/auto-login.guard.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { filter, take, map } from 'rxjs/operators';
+import { SupabaseService } from '../services/supabase.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutoLoginGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private supabaseService: SupabaseService, private router: Router) { }
+
+  canLoad(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.supabaseService.getCurrentUser().pipe(
+      filter(val => val !== null), // Filter out initial Behaviour subject value
+      take(1), // Otherwise the Observable doesn't complete!
+      map(isAuthenticated => {
+        if (isAuthenticated) {
+          return this.router.createUrlTree(['/inside']);
+        } else {
+          return true;
+        }
+      })
+    );
   }
 }
