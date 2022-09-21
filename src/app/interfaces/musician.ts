8f8cb80fa016213430832333118367c41bbb3aa6
chris-3 <choeftberger@gmail.com>
2022-09-21 14:29:30 +0200

    musicians Details added

--- src/app/interfaces/musician.ts
+++ src/app/interfaces/musician.ts
@@ -6,11 +6,11 @@ export interface Musician {
     id: number,
     first_name: string,
     last_name: string,
-    tel_number?: number,
+    phone?: number,
     street_address?: string,
     city?: string,
     postal_code?: number,
     creator_id: string,
     created_at: Date,
-    linked_acc: string,
+    email?: string,
 }
--- src/app/pages/musician/musician-details/musician-details.page.ts
+++ src/app/pages/musician/musician-details/musician-details.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from 'src/app/services/supabase.service';
+import { Musician, TABLE_MUSICIANS } from './../../../interfaces/musician';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-musician-details',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicianDetailsPage implements OnInit {
 
-  constructor() { }
+  public musician: Musician;
 
-  ngOnInit() {
+  sub1: Subscription;
+
+  constructor(
+    private dataService: SupabaseService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  async ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.musician = await this.dataService.getDataDetails(TABLE_MUSICIANS, +id);
   }
+  
+  updateMusician(){}
 
 }
